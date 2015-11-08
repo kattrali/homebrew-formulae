25b7625f43545a5662df28c764de3018c4742300
Delisa Mason <iskanamagus@gmail.com>
2015-11-07 20:06:20 -0800

    Tag pbhistory release

--- Formula/pbhistory.rb
+++ Formula/pbhistory.rb
@@ -1,7 +1,8 @@
-class PBHistory < Formula
+class Pbhistory < Formula
 
   homepage 'https://github.com/kattrali/pbhistory'
-  head 'https://github.com/kattrali/pbhistory'
+  head 'https://github.com/kattrali/pbhistory', :tag => '0.1.0'
+  sha256 '9fac575bcedc83216d7d38c4cb03bfc47ff97713a4f4d2cfcd1c1b23af35251a'
 
   depends_on 'kylef/formulae/conche' => :build
 
