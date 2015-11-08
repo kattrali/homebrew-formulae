bfac1a8965da4cb6590136599b6712e4d5b0060e
Delisa Mason <iskanamagus@gmail.com>
2015-11-07 22:17:05 -0800

    Add tagged release to thyme

--- Formula/thyme.rb
+++ Formula/thyme.rb
@@ -2,6 +2,8 @@ class Thyme < Formula
 
   homepage 'https://github.com/kattrali/thyme'
   head 'https://github.com/kattrali/thyme', :tag => '1.1.0'
+  url 'https://github.com/kattrali/thyme/archive/1.1.0.tar.gz'
+  sha256 '6a0828a92c8cfc4be7f6ca96d59c2db68dc2d42286ad2a5b79000a5f82f76d3d'
 
   depends_on 'rust' => :build
   depends_on 'ncurses'
