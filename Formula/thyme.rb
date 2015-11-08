41f39da0694382a1e61f3db85945059d53573457
Delisa Mason <iskanamagus@gmail.com>
2015-11-07 23:07:07 -0800

    Fix checksum for thyme 1.1.1

--- Formula/thyme.rb
+++ Formula/thyme.rb
@@ -3,7 +3,7 @@ class Thyme < Formula
   homepage 'https://github.com/kattrali/thyme'
   head 'https://github.com/kattrali/thyme', :tag => '1.1.1'
   url 'https://github.com/kattrali/thyme/archive/1.1.1.tar.gz'
-  sha256 '80ed49e73c82c3fd326fef6c79d62d30feb64a98fa6c5cec658c7a69a451241f'
+  sha256 'cb52655744c69d2502afda155a2dfb8f263530b8707ba6fe0b2b73c33f78f3a0'
 
   depends_on 'rust' => :build
   depends_on 'ncurses'
