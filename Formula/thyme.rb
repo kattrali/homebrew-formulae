fb0ad8843c09037bf65b31cad613c486886643d3
Delisa Mason <iskanamagus@gmail.com>
2015-11-07 19:53:38 -0800

    Tag latest thyme

--- Formula/thyme.rb
+++ Formula/thyme.rb
@@ -1,7 +1,7 @@
 class Thyme < Formula
 
   homepage 'https://github.com/kattrali/thyme'
-  head 'https://github.com/kattrali/thyme'
+  head 'https://github.com/kattrali/thyme', :tag => '1.1.0'
 
   depends_on 'rust' => :build
   depends_on 'ncurses'
