b3df4032b9d7fce573e292c8c4e041fcd7156696
Delisa Mason <iskanamagus@gmail.com>
2015-11-07 23:00:41 -0800

    Release thyme 1.1.1

--- Formula/thyme.rb
+++ Formula/thyme.rb
@@ -1,9 +1,9 @@
 class Thyme < Formula
 
   homepage 'https://github.com/kattrali/thyme'
-  head 'https://github.com/kattrali/thyme', :tag => '1.1.0'
-  url 'https://github.com/kattrali/thyme/archive/1.1.0.tar.gz'
-  sha256 '6a0828a92c8cfc4be7f6ca96d59c2db68dc2d42286ad2a5b79000a5f82f76d3d'
+  head 'https://github.com/kattrali/thyme', :tag => '1.1.1'
+  url 'https://github.com/kattrali/thyme/archive/1.1.1.tar.gz'
+  sha256 '80ed49e73c82c3fd326fef6c79d62d30feb64a98fa6c5cec658c7a69a451241f'
 
   depends_on 'rust' => :build
   depends_on 'ncurses'
