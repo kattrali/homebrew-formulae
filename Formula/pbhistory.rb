bbd284d3f77119133dd37843bac7a35a65240fc5
Delisa Mason <iskanamagus@gmail.com>
2015-11-07 20:25:01 -0800

    Fix pbhistory checksum for 0.1.0

--- Formula/pbhistory.rb
+++ Formula/pbhistory.rb
@@ -2,7 +2,8 @@ class Pbhistory < Formula
 
   homepage 'https://github.com/kattrali/pbhistory'
   head 'https://github.com/kattrali/pbhistory', :tag => '0.1.0'
-  sha256 '9fac575bcedc83216d7d38c4cb03bfc47ff97713a4f4d2cfcd1c1b23af35251a'
+  url 'https://github.com/kattrali/pbhistory/archive/0.1.0.tar.gz'
+  sha256 '91007e8ae2bfd171bd2a1f5708fd368aeb595d7070df6ff9a251d34262399ddb'
 
   depends_on 'kylef/formulae/conche' => :build
 
